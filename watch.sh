eb124d1ed0812d97c4390753df33fbb967322734
brycieboy <github.4200x@simplelogin.co>
2023-07-14 15:30:22 -0400

    Update

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 !#/bin/bash
 
-cd  ~/Sites/quartz; nodemon -w ~/Documents/The_Cosmos -w ~/Documents/quartz/assets/js -w ~/Documents/quartz/styles -w ~/Documents/quartz/layouts -w ~/Documents/quartz/config.toml -w ~/Documents/quartz/data/config.yaml -x "~/Documents/quartz/compile.sh" -e md,html,js,scss,xml
+cd  ~/Documents/quartz; nodemon -w ~/Documents/The_Cosmos -w ~/Documents/quartz/assets/js -w ~/Documents/quartz/styles -w ~/Documents/quartz/layouts -w ~/Documents/quartz/config.toml -w ~/Documents/quartz/data/config.yaml -x "~/Documents/quartz/compile.sh" -e md,html,js,scss,xml
